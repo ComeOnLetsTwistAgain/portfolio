7d43f1f81df2256a7459cfc0908c33724aac2b69
Antoine Souesme <antoine.souesme@outlook.com>
2017-01-29 17:55:39 +0100

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,76 @@
+'use strict';
+
+module.exports = function (grunt) {
+	var serveStatic = require('serve-static');
+
+	grunt.initConfig({
+		connect: {
+			options: {
+				port: 9000,
+				// Change this to '0.0.0.0' to access the server from outside.
+				hostname: 'localhost',
+				livereload: 35729,
+				base: {
+					path: './',
+					options: {
+						index: 'index.html'
+					}
+				}
+			},
+			livereload: {
+				options: {
+					open: true,
+					middleware: function (connect) {
+						return [
+							serveStatic('./')
+						];
+					}
+				}
+			},
+		},
+
+		watch: {
+			livereload: {
+				options: {
+					livereload: '<%= connect.options.livereload %>'
+				},
+				files: [
+					'./{,*/}*.html',
+					'./stylesheets/{,*/}*.css',
+					'./img/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+				]
+			},
+			lessWatch: {
+				files: "./less/*.less",
+				tasks: ["less"],
+				options: {
+					livereload: '<%= connect.options.livereload %>'
+				}
+			},
+		},
+
+
+
+		less: {
+			development: {
+                options: {
+                    paths: ["./less/"],
+                },
+                files: {
+                    "./stylesheets/style.css": "./less/style.less"
+                }
+            },
+		},
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-less');
+
+
+
+	// register tasks
+	grunt.registerTask('default', ['connect:livereload', 'watch']);
+
+};

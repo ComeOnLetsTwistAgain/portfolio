60bb6ec794c4fd9b3e3f5e9b50ac5bf2c657aac0
Antoine Souesme <antoine.souesme@outlook.com>
2017-02-14 19:51:59 +0100

    Added material design tweaks + navbar layout

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,7 +1,33 @@
 $(document).ready(() => {
+	$('.btn-send-message').attr('disabled', 'disabled');
+
+	$('#mail-form input, #mail-form textarea').keydown(event => {
+		const name = $('#name').val();
+		const email = $('#email').val();
+		const phone = $('#phone').val();
+		const message = $('#message').val();
+
+
+		if (name !== "" && email !== "" && phone !== "" && message !== "") {
+			$('.btn-send-message').removeAttr('disabled');
+		}
+	})
+
+	/**
+	 * Vide les champs du formulaire
+	 */
+	var emptyFields = () => {
+		$('#name').val('');
+		$('#email').val('');
+		$('#phone').val('');
+		$('#message').val('');
+	};
 
 
 
+	/**
+	 * 
+	 */
 	$('#submit-contact').click((event) => {
 		const name = $('#name').val();
 		const email = $('#email').val();
@@ -20,12 +46,23 @@ $(document).ready(() => {
 			}
 		});
 
+		// emptying the form fields
+		emptyFields();
+
+		$('.btn-send-message').attr('disabled', 'disabled');
+		$('.spinner-sub-container').fadeIn();
+
 		request.done((msg) => {
-			console.log("Request sent, ", msg);
+			$('.mdl-spinner').hide();
+			$('.material-icons.checked').fadeIn();
+
+			setTimeout(() => {
+				$('.spinner-sub-container').fadeOut();
+			}, 4000);
 		});
 
 		request.fail((jqXHR, textStatus) => {
-			alert("Request failed: " + textStatus);
+			console.log("Request failed: " + textStatus);
 		});
 
 
